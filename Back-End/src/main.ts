c8a4f5cf2082bbbc1476394beb527073b577c1a8
Ahmed10257 <ahmed.mansour10257@gmail.com>
2024-06-17 08:25:14 +0300

    docs:added documented APIs using swagger

--- Back-End/src/main.ts
+++ Back-End/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,16 @@ async function bootstrap() {
     origin: 'http://localhost:4200',
     credentials: true,
   });
+  //Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  //Create the Swagger document
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
--- Back-End/src/students/students.controller.ts
+++ Back-End/src/students/students.controller.ts
@@ -3,36 +3,54 @@ import { StudentsService } from './students.service';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { AuthGuard } from 'src/auth/Guards/auth.guard';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 
 /* the controller is responsible for handling incoming requests and returning responses to the client
 and it's protected by the AuthGuard */
 @UseGuards(AuthGuard)
+@ApiTags('students')
 @Controller('students')
 export class StudentsController {
   constructor(private readonly studentsService: StudentsService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new student' })
+  @ApiResponse({ status: 201, description: 'The student has been successfully created.' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   create(@Body() createStudentDto: CreateStudentDto) {
     return this.studentsService.create(createStudentDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Get all students' })
+  @ApiResponse({ status: 200, description: 'The students have been successfully fetched.' })
+  @ApiResponse({ status: 404, description: 'Students not found.' })
   findAll() {
     return this.studentsService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Get a student by ID' })
+  @ApiResponse({ status: 200, description: 'The student has been successfully fetched.' })
+  @ApiResponse({ status: 404, description: 'Student not found.' })
   findOne(@Param('id') id: string) {
     return this.studentsService.findOne(+id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update a student by ID' })
+  @ApiResponse({ status: 200, description: 'The student has been successfully updated.' })
+  @ApiResponse({ status: 404, description: 'Student not found.' })
   update(@Param('id') id: string, @Body() updateStudentDto: UpdateStudentDto) {
     return this.studentsService.update(+id, updateStudentDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a student by ID' })
+  @ApiResponse({ status: 200, description: 'The student has been successfully deleted.' })
+  @ApiResponse({ status: 404, description: 'Student not found.' })
   remove(@Param('id') id: string) {
     return this.studentsService.remove(+id);
   }
