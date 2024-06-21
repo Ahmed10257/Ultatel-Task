160263282ddac7f144caa409ed96ab5e69c97de0
Ahmed10257 <ahmed.mansour10257@gmail.com>
2024-06-21 11:09:01 +0300

    removed the footer

--- Front-End/src/app/app.component.ts
+++ Front-End/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Component } from '@angular/core';
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import { jwtInterceptor } from './core/interceptors/jwt.interceptor';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
-import { FooterComponent } from './Components/footer/footer.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, HttpClientModule, NavBarComponent, FooterComponent],
+  imports: [RouterOutlet, RouterModule, HttpClientModule, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [{ provide: HTTP_INTERCEPTORS, useValue: jwtInterceptor, multi: true }]
